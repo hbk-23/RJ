a6ef8e31cb885f8cd63014d11264a9a2163069f1
hbk-23 <0424.hibiki@gmail.com>
2023-06-28 14:07:45 +0900

    0628

--- 5.py
+++ 5.py
@@ -15,14 +15,14 @@ def draw_stick_figure(canvas, x, y):
     canvas.create_line(x+25, y+100, x+75, y+150, fill='black', width=2)
 
 def move_stick_figure():
-    global x_position
+    global x_position, x_direction
     canvas.delete("all")  # キャンバス上の描画を全て削除
-    x_position += 5  # 移動量を指定
+    x_position += x_direction * 5  # 移動量を指定
+    # x方向の折り返し処理
+    if x_position >= 400-75 or x_position <= 25:
+        x_direction *= -1
     draw_stick_figure(canvas, x_position, 100)
-    window.update()  # ウィンドウの状態を更新
-    # 移動を続けるための条件
-    if x_position < 350:
-        window.after(100, move_stick_figure)
+    window.after(50, move_stick_figure)
 
 # ウィンドウの作成
 window = tk.Tk()
@@ -33,6 +33,7 @@ canvas.pack()
 
 # 棒人間の描画
 x_position = 150
+x_direction = 1
 draw_stick_figure(canvas, x_position, 100)
 
 # 棒人間の移動
