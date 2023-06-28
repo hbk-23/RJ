39e022d168d1ec243e2ebabf40cbd7d89d5cd331
hbk-23 <0424.hibiki@gmail.com>
2023-06-28 13:57:50 +0900

    0628

--- 5.py
+++ 5.py
@@ -14,6 +14,16 @@ def draw_stick_figure(canvas, x, y):
     # 右足
     canvas.create_line(x+25, y+100, x+75, y+150, fill='black', width=2)
 
+def move_stick_figure():
+    global x_position
+    canvas.delete("all")  # キャンバス上の描画を全て削除
+    x_position += 5  # 移動量を指定
+    draw_stick_figure(canvas, x_position, 100)
+    window.update()  # ウィンドウの状態を更新
+    # 移動を続けるための条件
+    if x_position < 350:
+        window.after(100, move_stick_figure)
+
 # ウィンドウの作成
 window = tk.Tk()
 
@@ -22,8 +32,11 @@ canvas = tk.Canvas(window, width=400, height=400)
 canvas.pack()
 
 # 棒人間の描画
-draw_stick_figure(canvas, 150, 100)
-canvas.move()
+x_position = 150
+draw_stick_figure(canvas, x_position, 100)
+
+# 棒人間の移動
+move_stick_figure()
 
 # ウィンドウのメインループ
 window.mainloop()
